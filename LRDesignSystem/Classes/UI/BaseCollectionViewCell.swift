550a9b4eefe03b97aceefb51ad0e7aa0a3768253
Pedro Manfredi <barcelo.sk8@gmail.com>
2020-04-16 13:38:10 +0200

    0.5.2

--- LRDesignSystem/Classes/UI/BaseCollectionViewCell.swift
+++ LRDesignSystem/Classes/UI/BaseCollectionViewCell.swift
@@ -1,16 +1,1 @@
-//
-//  BaseCollectionViewCell.swift
-//  LRDesignSystem
-//
-//  Created by Pedro Manfredi on 16/04/2020.
-//
 
-import Foundation
-import RxSwift
-class BaseCollectionViewCell: UICollectionViewCell {
-    var disposeBag = DisposeBag()
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        disposeBag = DisposeBag()
-    }
-}
