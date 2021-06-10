5ad6ccb66a023acce554e7327bee13830ea5b471
Alqueraf <alqueraf@gmail.com>
2021-06-11 00:29:45 +0200

    Add fonts to target

--- Package.swift
+++ Package.swift
@@ -22,7 +22,30 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "LRDesignSystem", dependencies: []),
+            name: "LRDesignSystem",
+            dependencies: [],
+            resources: [
+                .process("Fonts/Lato-Black"),
+                .process("Fonts/Lato-BlackItalic"),
+                .process("Fonts/Lato-Bold"),
+                .process("Fonts/Lato-BoldItalic"),
+                .process("Fonts/Lato-Light"),
+                .process("Fonts/Lato-LightItalic"),
+                .process("Fonts/Lato-Regular"),
+                .process("Fonts/Lato-RegularItalic"),
+                .process("Fonts/Lato-Thin"),
+                .process("Fonts/Lato-ThinItalic"),
+                .process("Fonts/MarkOffcPro-Bold"),
+                .process("Fonts/MarkOffcPro-BoldItalic"),
+                .process("Fonts/MarkOffcPro-Book"),
+                .process("Fonts/MarkOffcPro-BookItalic"),
+                .process("Fonts/MarkOffcPro-CondHeavy"),
+                .process("Fonts/MarkOffcPro-Heavy"),
+                .process("Fonts/MarkOffcPro-HeavyItalic"),
+                .process("Fonts/MarkOffcPro-Light"),
+                .process("Fonts/MarkOffcPro-LightItalic"),
+            ]
+        ),
         .testTarget(
             name: "LRDesignSystemTests",
             dependencies: ["LRDesignSystem"]),
